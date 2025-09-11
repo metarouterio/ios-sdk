bac5eb6b4be4d7fe12c7b2cad3248ebbf02bc0fb
Christopher Houdlette <choudlette@gmail.com>
2025-09-11 09:32:10 -0600

    removing comments from AnalyticsInterface

--- Sources/MetaRouter/analytics/AnalyticsInterface.swift
+++ Sources/MetaRouter/analytics/AnalyticsInterface.swift
@@ -1,23 +1,18 @@
 import Foundation
 
 public protocol AnalyticsInterface: AnyObject, Sendable {
-    // Track methods - overloaded to allow calling without properties
     func track(_ event: String, properties: [String: CodableValue]?)
     func track(_ event: String)
 
-    // Identify methods - overloaded to allow calling without traits
     func identify(_ userId: String, traits: [String: CodableValue]?)
     func identify(_ userId: String)
 
-    // Group methods - overloaded to allow calling without traits
     func group(_ groupId: String, traits: [String: CodableValue]?)
     func group(_ groupId: String)
 
-    // Screen methods - overloaded to allow calling without properties
     func screen(_ name: String, properties: [String: CodableValue]?)
     func screen(_ name: String)
 
-    // Page methods - overloaded to allow calling without properties
     func page(_ name: String, properties: [String: CodableValue]?)
     func page(_ name: String)
 
