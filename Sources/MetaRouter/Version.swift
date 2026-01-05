a3d8c416b327f04738137c75a63b8b7fd4529297
Christopher Houdlette <choudlette@gmail.com>
2026-01-05 13:15:36 -0700

    fix: updating version of library in device info

---
+++ Sources/MetaRouter/Version.swift
@@ -0,0 +1,5 @@
+public enum MetaRouterSDK {
+    // IMPORTANT: Update this value before each release.
+    // This version is included in the `library.version` context field of all events.
+    public static let version = "1.2.1"
+}
