ce693667ce96765eb1b122bdf9b84747243ad16a
Christopher Houdlette <choudlette@gmail.com>
2026-01-30 10:50:42 -0700

    updating version string

--- Sources/MetaRouter/Version.swift
+++ Sources/MetaRouter/Version.swift
@@ -1,5 +1,5 @@
 public enum MetaRouterSDK {
     // IMPORTANT: Update this value before each release.
     // This version is included in the `library.version` context field of all events.
-    public static let version = "1.2.1"
+    public static let version = "1.2.2"
 }
